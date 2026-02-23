3208667ddebc99f14dcfa971c67a71b94e9faedc
vbnn <vbnn2015@gmail.com>
2026-02-23 09:17:13 +0300

    dobavi l4ikli

--- main.js
+++ main.js
@@ -9,16 +9,26 @@ const personalMovieDB = {
   genres: [],
   privat: "false",
 };
-const posledniProsmotreniy = prompt(
-  "Один из последних просмотренных фильмов?",
-  "",
-);
-const o4nkaFilma = prompt("На сколько оцените его?", "");
 
-const a = prompt("Один из последних просмотренных фильмов?", "");
-const b = prompt("На сколько оцените его?", "");
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних просмотренных фильмов?", "");
+  const b = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[posledniProsmotreniy] = o4nkaFilma;
-personalMovieDB.movies[a] = b;
-console.log(personalMovieDB);
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("sdelano");
+  } else {
+    console.log("error");
+    i--; //если это условия не выполнилось то вернется назад на одну итерацию
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log(" Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+  console.log("Вы киноман");
+} else console.log("Пороизошла ошибка");
+
 console.log(personalMovieDB);
